c553b11a3c59eb0b98ae43ed71aa298876cff040
kamalsroor1 <kamal@softoio.com>
2026-04-18 23:19:04 +0200

    feat: define authenticated application routes and Livewire components

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Auth\Login;
 use App\Livewire\Products\ProductList;
+use App\Livewire\Inventory\IngredientList;
 use App\Livewire\Dashboard\Index as Dashboard;
 
 Route::get('/login', Login::class)->name('login');
@@ -17,6 +18,10 @@ Route::middleware('auth')->group(function () {
 
     Route::middleware('permission:manage products')->group(function () {
         Route::get('/products', ProductList::class)->name('products.index');
+    });
+
+    Route::middleware('permission:manage ingredients')->group(function () {
+        Route::get('/inventory', IngredientList::class)->name('inventory.index');
     });
 
     Route::get('/pos', function () {
