6a6372aa4d17cd1f1b102aa169448308eb93378f
Kamal Sroor <kamal.sroor@travware.com>
2026-04-19 17:35:02 +0200

    Model: Create custom Role model with HasUuids for Spatie Permission

---
+++ app/Models/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Permission\Models\Role as SpatieRole;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Role extends SpatieRole
+{
+    use HasUuids;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+}
