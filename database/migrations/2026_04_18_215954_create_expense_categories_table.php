f22e6acabbea17fd89a588899d4627aa4d3ff777
Kamal Sroor <kamal.sroor@travware.com>
2026-04-19 17:34:40 +0200

    Migration: Convert expense_categories table to use UUIDs

--- database/migrations/2026_04_18_215954_create_expense_categories_table.php
+++ database/migrations/2026_04_18_215954_create_expense_categories_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('expense_categories', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name')->unique();
             $table->string('description')->nullable();
             $table->timestamps();
