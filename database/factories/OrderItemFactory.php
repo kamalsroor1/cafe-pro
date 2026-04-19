06ddc62117f3d13f3c9bb948b46d54a724f1c34f
kamalsroor1 <teko.2031@gmail.com>
2026-04-19 18:20:09 +0200

    test: add OrderItemFactory

---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<OrderItem>
+ */
+class OrderItemFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'order_id' => Order::factory(),
+            'product_id' => Product::factory(),
+            'quantity' => $this->faker->numberBetween(1, 10),
+            'unit_price' => 10.00,
+            'subtotal' => 10.00, // Should be calculated usually
+        ];
+    }
+}
