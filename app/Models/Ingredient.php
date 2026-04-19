a03463834226e8885593d9b1036348fb40e1db94
Kamal Sroor <kamal.sroor@travware.com>
2026-04-19 17:35:11 +0200

    Model: Add HasUuids to Ingredient model and use custom pivot for products

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -2,23 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ingredient extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, HasUuids, SoftDeletes;
 
     protected $guarded = [];
 
     public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class)
-                    ->withPivot('amount_needed')
-                    ->withTimestamps();
+            ->using(ProductIngredient::class)
+            ->withPivot('amount_needed')
+            ->withTimestamps();
     }
 
     public function wastages(): HasMany
