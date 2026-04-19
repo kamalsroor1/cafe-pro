7739db7aef7cccb592136c4bdfb66d110d5bdd74
Kamal Sroor <kamal.sroor@travware.com>
2026-04-19 17:35:20 +0200

    Model: Add HasUuids to Payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use App\Enums\PaymentMethod;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $guarded = [];
 
