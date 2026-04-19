dda05e2a1b9eb5da999c059e99f370bb2667a6d0
Kamal Sroor <kamal.sroor@travware.com>
2026-04-19 17:35:02 +0200

    Model: Create custom Permission model with HasUuids for Spatie Permission

---
+++ app/Models/Permission.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Permission\Models\Permission as SpatiePermission;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Permission extends SpatiePermission
+{
+    use HasUuids;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+}
