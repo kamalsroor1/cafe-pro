1b2d4b96b8719adb16acc4f3c166f02b8e6d45f0
kamalsroor1 <teko.2031@gmail.com>
2026-04-19 18:20:09 +0200

    test: add CategoryFactory with UUID support

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $name = $this->faker->unique()->word(),
+            'slug' => Str::slug($name),
+            'description' => $this->faker->sentence(),
+            'is_active' => true,
+        ];
+    }
+}
